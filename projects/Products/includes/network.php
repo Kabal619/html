ce286a5279be7c23bf95bcec4cc3053de76f02bf
Kabal619 <random619@outlook.com>
2016-04-11 19:04:16 -0700

    Revert "Added files via upload"
    This reverts commit a5f5b9c39d70543724431a8702a4a3f9962e1a8c.

--- projects/Products/includes/network.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$network = mysql_connect("localhost", "roots", "link12");
-
-	
-	if (!$network)
-		die('Could not connect to db' . mysql_error());
-		
-?>
--- projects/widget_corp/includes/constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//Database Constants
-define("DB_SERVER", "localhost");
-define("DB_USER", "juliegri_AColmen");
-define("DB_PASS", "0556200");
-define("DB_NAME", "juliegri_AColmenero");
